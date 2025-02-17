660bcf24295031541d6acbc25af9b064c4a2e214
Adrian Borup <adrian@borup.com>
2025-02-17 09:51:47 +0100

    Format edges

--- src/weighted_push_relabel.py
+++ src/weighted_push_relabel.py
@@ -38,6 +38,9 @@ class Edge:
     def __hash__(self):
         return hash(self.id)
 
+    def __str__(self):
+        return f"{self.u} -({self.c})> {self.v}"
+
 
 @dataclass
 class WeightedPushRelabel:
