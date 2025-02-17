a1623e5e26e2c2cfab501877319a260b0944f2bb
albe2669 <albert@risenielsen.dk>
2025-02-17 10:06:22 +0100

    expect tuple

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from src.weighted_push_relabel import Edge, Graph
 from typing import Callable
 from tests.flows.utils import make_test_flow_input
@@ -32,14 +33,15 @@ def parse_input(
 
 def wrap_correct(
     g: Graph, c: list[int], sources: list[int], sinks: list[int], w: int, h: int
-) -> int:
+) -> tuple[int, defaultdict[Edge, int] | None]:
     edges, capacities, s, t = make_test_flow_input(g, c, sources, sinks, w, h)
 
-    return find_max_flow_correct(edges, capacities, s=s, t=t)
+    return (find_max_flow_correct(edges, capacities, s=s, t=t), None)
 
 
 FlowFn = Callable[
-    [Graph, list[int], list[int], list[int], Callable[[Edge], int], int], int
+    [Graph, list[int], list[int], list[int], Callable[[Edge], int], int],
+    tuple[int, defaultdict[Edge, int] | None],
 ]
 
 
@@ -49,5 +51,5 @@ def run_test(
     flow_fn: FlowFn,
 ):
     g, c, sources, sinks = parse_input(input, expected)
-    mf = flow_fn(g, c, sources, sinks, lambda e: 1, 6 * 6)
+    mf, _ = flow_fn(g, c, sources, sinks, lambda e: 1, 6 * 6)
     assert mf == expected, f"Expected {expected}, got {mf}"
