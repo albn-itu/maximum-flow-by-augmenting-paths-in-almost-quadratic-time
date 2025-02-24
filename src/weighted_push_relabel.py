7e1bfb493150895cc2fdcd94d807e71fdaf3960a
Adrian Borup <adrian@borup.com>
2025-02-24 12:16:35 +0100

    Move benchmark registration to before murder

--- src/weighted_push_relabel.py
+++ src/weighted_push_relabel.py
@@ -58,13 +58,13 @@ class WeightedPushRelabel:
 
             l[v] = min(next_multiple_of(n=l[v], multiple_of=w(e)) for e in edges)
 
-            if l[v] > 9 * h:
-                self.mark_dead(v)
-                return
-
             benchmark.register_or_update(
                 "blik.highest_level", l[v], lambda x: max(x, l[v])
             )
+
+            if l[v] > 9 * h:
+                self.mark_dead(v)
+                return
 
             for e in (e for e in edges if l[v] % w(e) == 0):
                 x, y = e.start(), e.end()
