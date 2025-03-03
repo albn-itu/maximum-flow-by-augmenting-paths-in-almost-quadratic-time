ddde07e68b92b301d54b2406e2877e2b56582e33
albe2669 <albert@risenielsen.dk>
2025-03-03 10:05:17 +0100

    Add the maxflow data

---
+++ tests/scripts/test_dag.py
@@ -0,0 +1,54 @@
+import os
+from networkx import is_directed_acyclic_graph, MultiDiGraph
+
+
+def parse_input(input: str) -> list[tuple[int, int]]:
+    lines = input.strip().split("\n")
+    n, _, s, t = map(int, lines[0].split())
+
+    edges: list[tuple[int, int]] = []
+
+    for line in lines[1:]:
+        u, rest = line.split("-(")
+        _, v = rest.split(")>")
+
+        edges.append((int(u), int(v)))
+
+    return edges
+
+
+def get_graph(input_file: str) -> list[tuple[int, int]]:
+    with open(input_file, "r") as f:
+        content = f.read()
+
+    return parse_input(content)
+
+
+def test_if_dag(input_file: str) -> bool:
+    edges = get_graph(input_file)
+    g = MultiDiGraph(edges)
+
+    return is_directed_acyclic_graph(g)
+
+
+if __name__ == "__main__":
+    base_path = "./tests/data/maxflow"
+    files = os.listdir(base_path)
+
+    is_dag: list[str] = []
+    not_dag: list[str] = []
+
+    for file in files:
+        if file == ".gitignore":
+            continue
+        file = os.path.join(base_path, file)
+
+        if test_if_dag(file):
+            is_dag.append(file)
+        else:
+            not_dag.append(file)
+
+    print("Is DAG:")
+    print("\n".join(is_dag))
+    print("Not DAG:")
+    print("\n".join(not_dag))
--- tests/test_data.py
+++ tests/test_data.py
@@ -54,8 +54,7 @@ def read_file(file: str) -> str:
 
 
 def run_test(input_file: str, expected: int, flow_fn: FlowFn):
-    benchmark.start_benchmark(os.environ.get("PYTEST_CURRENT_TEST") or "unknown")
-    benchmark.register_or_update("bench_config.file", input_file, lambda x: x)
+    benchmark.register("bench_config.file", input_file)
 
     input_str = read_file(input_file)
     run_test_str(input_str, expected, flow_fn)
@@ -106,59 +105,181 @@ def test_correct_line(input_file: str, expected: int):
     run_test(input_file, expected, wrap_correct)
 
 
-def get_maxflow_files(small: bool) -> list[tuple[str, int]]:
-    files = os.listdir("tests/data/maxflow")
-    file_expected = []
-
-    for file in files:
-        if not file.endswith(".txt"):
-            continue
-
-        _, _, _, expected = file.split("_")
-        expected = int(expected.split(".")[0])
-        if small and int(expected) > 10**5:
-            continue
-
-        file_expected.append(
-            pytest.param(f"tests/data/maxflow/{file}", expected, id=file)
-        )
-
-    return file_expected
-
-
-@pytest.mark.paper
-@pytest.mark.maxflow
-@pytest.mark.parametrize("input_file,expected", get_maxflow_files(True))
-def test_flow_maxflow_small(input_file: str, expected: int):
-    run_test(input_file, expected, weighted_push_relabel)
-
-
-@pytest.mark.correct
-@pytest.mark.maxflow
-@pytest.mark.parametrize("input_file,expected", get_maxflow_files(True))
-def test_correct_maxflow_small(input_file: str, expected: int):
-    run_test(input_file, expected, wrap_correct)
-
-
-@pytest.mark.paper
-@pytest.mark.large
-@pytest.mark.maxflow
-@pytest.mark.parametrize("input_file,expected", get_maxflow_files(False))
-def test_flow_maxflow_large(input_file: str, expected: int):
-    run_test(input_file, expected, weighted_push_relabel)
-
-
-@pytest.mark.correct
-@pytest.mark.large
-@pytest.mark.maxflow
-@pytest.mark.parametrize("input_file,expected", get_maxflow_files(False))
-def test_correct_maxflow_large(input_file: str, expected: int):
-    run_test(input_file, expected, wrap_correct)
-
-
 @pytest.mark.benchmark
 @pytest.mark.parametrize("input_file,expected", DAG_INPUT_EXPECTED)
 @bench
 def test_benchmark_dag(input_file: str, expected: int):
     run_test_with_topsort(read_file(input_file), expected, weighted_push_relabel)
+    run_test(input_file, expected, wrap_correct)
+
+
+def parse_maxflow_file_names(files: list[str]) -> list[tuple[str, int, str]]:
+    file_expected: list[tuple[str, int, str]] = []
+
+    for file in files:
+        base_name = os.path.basename(file)
+
+        _, _, _, expected = base_name.split("_")
+        expected = int(expected.split(".")[0])
+
+        file_expected.append(pytest.param(file, expected, id=base_name))
+
+    return file_expected
+
+
+MAXFLOW_DAG_FILES = [
+    "tests/data/maxflow/000_4_5_3.txt",
+    "tests/data/maxflow/000_2_1_100000.txt",
+    "tests/data/maxflow/001_2_1_0.txt",
+    "tests/data/maxflow/002_4_5_3.txt",
+    "tests/data/maxflow/003_498_497_328891.txt",
+    "tests/data/maxflow/004_499_498_373603.txt",
+    "tests/data/maxflow/005_498_497_328891.txt",
+    "tests/data/maxflow/006_499_498_373603.txt",
+    "tests/data/maxflow/007_499_698_38465926.txt",
+    "tests/data/maxflow/008_404_805_165635950.txt",
+    "tests/data/maxflow/009_404_805_165635950.txt",
+    "tests/data/maxflow/009_499_698_38465926.txt",
+    "tests/data/maxflow/010_479_955_67108864.txt",
+    "tests/data/maxflow/010_404_805_165635950.txt",
+    "tests/data/maxflow/011_404_805_165635950.txt",
+    "tests/data/maxflow/011_479_955_67108864.txt",
+    "tests/data/maxflow/012_481_959_111126052.txt",
+    "tests/data/maxflow/012_479_955_67108864.txt",
+    "tests/data/maxflow/013_479_955_67108864.txt",
+    "tests/data/maxflow/013_484_965_91034320.txt",
+    "tests/data/maxflow/014_487_971_145586566.txt",
+    "tests/data/maxflow/014_481_959_111126052.txt",
+    "tests/data/maxflow/015_488_973_111955490.txt",
+    "tests/data/maxflow/015_484_965_91034320.txt",
+    "tests/data/maxflow/016_487_971_145586566.txt",
+    "tests/data/maxflow/016_488_973_111955490.txt",
+    "tests/data/maxflow/017_490_977_67201230.txt",
+    "tests/data/maxflow/017_488_973_111955490.txt",
+    "tests/data/maxflow/018_498_993_114179816.txt",
+    "tests/data/maxflow/018_488_973_111955490.txt",
+    "tests/data/maxflow/019_490_977_67201230.txt",
+    "tests/data/maxflow/019_499_995_80508994.txt",
+    "tests/data/maxflow/020_500_997_68424164.txt",
+    "tests/data/maxflow/020_498_993_114179816.txt",
+    "tests/data/maxflow/021_499_995_80508994.txt",
+    "tests/data/maxflow/022_500_997_68424164.txt",
+]
+
+MAXFLOW_NOT_DAG_FILES = [
+    "tests/data/maxflow/001_468_490_1219139.txt",
+    "tests/data/maxflow/002_468_490_1340991.txt",
+    "tests/data/maxflow/003_468_490_1219139.txt",
+    "tests/data/maxflow/004_468_490_1340991.txt",
+    "tests/data/maxflow/005_492_591_28141590.txt",
+    "tests/data/maxflow/006_495_594_19452032.txt",
+    "tests/data/maxflow/008_495_594_19452032.txt",
+    "tests/data/maxflow/022_100_2273_920680583.txt",
+    "tests/data/maxflow/023_100_1700_518816803.txt",
+    "tests/data/maxflow/023_413_2370_184306415.txt",
+    "tests/data/maxflow/024_100_2273_920680583.txt",
+    "tests/data/maxflow/025_413_2370_184306415.txt",
+    "tests/data/maxflow/026_500_3824_24315761.txt",
+    "tests/data/maxflow/027_324_5687_679698373.txt",
+    "tests/data/maxflow/028_290_6667_953735136.txt",
+    "tests/data/maxflow/030_166_7642_1823874790.txt",
+    "tests/data/maxflow/031_500_8174_787664309.txt",
+    "tests/data/maxflow/034_327_9020_293978834.txt",
+    "tests/data/maxflow/035_500_9042_104658622.txt",
+    "tests/data/maxflow/036_483_9211_351476866.txt",
+    "tests/data/maxflow/037_359_9256_238314638.txt",
+    "tests/data/maxflow/038_500_9363_200000000.txt",
+    "tests/data/maxflow/039_304_9419_860960053.txt",
+    "tests/data/maxflow/040_500_9776_117452029.txt",
+    "tests/data/maxflow/041_500_9780_965640408.txt",
+    "tests/data/maxflow/042_500_9790_667639572.txt",
+    "tests/data/maxflow/043_500_9790_265142382.txt",
+    "tests/data/maxflow/044_500_9794_1462035113.txt",
+    "tests/data/maxflow/045_500_9815_1070732820.txt",
+    "tests/data/maxflow/046_500_9900_854026659.txt",
+    "tests/data/maxflow/047_499_9901_1027542608.txt",
+    "tests/data/maxflow/048_499_9908_1262307361.txt",
+]
+
+
+@pytest.mark.paper
+@pytest.mark.maxflow
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_DAG_FILES)
+)
+def test_flow_maxflow_dag(input_file: str, expected: int):
+    run_test(input_file, expected, weighted_push_relabel)
+
+
+@pytest.mark.paper
+@pytest.mark.maxflow
+@pytest.mark.w_top_sort
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_DAG_FILES)
+)
+def test_flow_maxflow_dag_with_topsort(input_file: str, expected: int):
+    run_test_with_topsort(read_file(input_file), expected, weighted_push_relabel)
+
+
+@pytest.mark.correct
+@pytest.mark.maxflow
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_DAG_FILES)
+)
+def test_correct_maxflow_dag(input_file: str, expected: int):
+    run_test(input_file, expected, wrap_correct)
+
+
+@pytest.mark.benchmark
+@pytest.mark.maxflow
+@bench
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_DAG_FILES)
+)
+def test_benchmark_maxflow_dag(input_file: str, expected: int):
+    run_test(input_file, expected, weighted_push_relabel)
+    run_test(input_file, expected, wrap_correct)
+
+
+@pytest.mark.benchmark
+@pytest.mark.maxflow
+@pytest.mark.w_top_sort
+@bench
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_DAG_FILES)
+)
+def test_benchmark_maxflow_dag_topsort(input_file: str, expected: int):
+    run_test_with_topsort(read_file(input_file), expected, weighted_push_relabel)
+    run_test(input_file, expected, wrap_correct)
+
+
+@pytest.mark.paper
+@pytest.mark.maxflow
+@pytest.mark.notdag
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_NOT_DAG_FILES)
+)
+def test_flow_maxflow_not_dag(input_file: str, expected: int):
+    run_test(input_file, expected, weighted_push_relabel)
+
+
+@pytest.mark.correct
+@pytest.mark.maxflow
+@pytest.mark.notdag
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_NOT_DAG_FILES)
+)
+def test_correct_maxflow_not_dag(input_file: str, expected: int):
+    run_test(input_file, expected, wrap_correct)
+
+
+@pytest.mark.benchmark
+@pytest.mark.maxflow
+@pytest.mark.notdag
+@bench
+@pytest.mark.parametrize(
+    "input_file,expected", parse_maxflow_file_names(MAXFLOW_NOT_DAG_FILES)
+)
+def test_benchmark_maxflow_not_dag(input_file: str, expected: int):
+    run_test(read_file(input_file), expected, weighted_push_relabel)
     run_test(input_file, expected, wrap_correct)
