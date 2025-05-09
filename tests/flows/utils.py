5d458e8597e914afe509cbf5041e1c5d0bda5fec
albe2669 <albert@risenielsen.dk>
2025-05-09 10:46:11 +0200

    Filter away cycles

--- tests/flows/utils.py
+++ tests/flows/utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Callable
 from src import benchmark
 from src.utils import Edge, Graph
@@ -38,21 +39,37 @@ def weight_function_from_flow(
     _ = pr_instance.max_flow(source, sink)
     flow = pr_instance.flow
 
+    dag_flow_edges = induced_flow_dag(flow)
+
     def weight_function(edge: Edge) -> int:
         u, v = edge.start(), edge.end()
 
-        default_weight = len(G.E) * 100
-        if (
-            u in flow and v in flow[u] and flow[u][v] > 0
-        ):  # If there is flow, return 0, as we want to use this edge
-            if (
-                v in flow and u in flow[v] and flow[v][u] > 0
-            ):  # If the reverse edge also has flow, then we only use the forward edge
-                if edge.forward:  # If the edge is forward, we return 0
-                    return 1
-                else:
-                    return default_weight
+        if (u, v) in dag_flow_edges:
             return 1
-        return default_weight
+
+        return len(G.V) * 100
 
     return weight_function
+
+
+def induced_flow_dag(
+    flow: defaultdict[int, defaultdict[int, int]],
+) -> set[tuple[int, int]]:
+    """
+    Create a new graph with only the edges that have flow in the original graph.
+    """
+
+    edges_to_keep: set[tuple[int, int]] = set()
+    seen_vertices: set[int] = set()
+
+    # Iterate the flow dictionary keeping all edges that don't induce a cycle
+    for u in flow:
+        seen_vertices.add(u)
+
+        for v in flow[u]:
+            if v in seen_vertices:
+                continue  # Keeping this edge would create a cycle
+            if flow[u][v] > 0:
+                edges_to_keep.add((u, v))
+
+    return edges_to_keep
