271e4e10517ebeec3c31545fe8cf50863ff0ffbd
albe2669 <albert@risenielsen.dk>
2025-05-17 20:09:30 +0200

    Expander multiplication

---
+++ tests/scripts/expander_multiplication.py
@@ -0,0 +1,51 @@
+import os
+
+from classic_push_relabel import PushRelabel
+
+from src.utils import Graph
+from src.expander_hierarchy_generator import ExpanderHierarchy, from_json_file
+from networkx.algorithms import hierarchy
+
+
+def multiply_graph(G: Graph, factor: int) -> Graph:
+    edges: list[tuple[int, int]] = []
+    caps: list[int] = []
+    for i, (u, v) in enumerate(G.E):
+        for j in range(factor):
+            edges.append((u, v))
+            caps.append(G.c[i])
+
+    return Graph(
+        V=G.V,
+        E=edges,
+        c=caps,
+    )
+
+
+def multiply_hierarchy(hierarchy: ExpanderHierarchy, factor: int) -> ExpanderHierarchy:
+    multiplied_g = multiply_graph(hierarchy.G, factor)
+    flow = PushRelabel(multiplied_g).max_flow(hierarchy.s, hierarchy.t)
+
+    return ExpanderHierarchy(
+        G=multiplied_g,
+        order=hierarchy.order,
+        components=hierarchy.components,
+        hierarchy=hierarchy.hierarchy,
+        s=hierarchy.s,
+        t=hierarchy.t,
+        flow=flow,
+    )
+
+
+if __name__ == "__main__":
+    dir = "tests/data/expander_hierarchies"
+    files = os.listdir(dir)
+    for file in files:
+        if not file.endswith(".json"):
+            continue
+
+        print(f"Processing {file}")
+        for factor in [2, 3]:
+            hierarchy = from_json_file(f"{dir}/{file}")
+            hierarchy = multiply_hierarchy(hierarchy, 3)
+            hierarchy.dump_to_json_file(f"{dir}/factor_{factor}_{file}")
